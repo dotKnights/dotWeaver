3fa7e145a6825e2872fb3f9973fd8614ce5587a3
Nacer Berkane <78866928+dotNacer@users.noreply.github.com>
2026-05-29 21:05:15 +0200

    feat: enable better-auth organization plugin

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { organization } from 'better-auth/plugins';
 import { prisma } from './prisma';
 import { env } from '$env/dynamic/private';
 
@@ -15,5 +16,6 @@ export const auth = betterAuth({
 			clientId: env.GITHUB_CLIENT_ID!,
 			clientSecret: env.GITHUB_CLIENT_SECRET!
 		}
-	}
+	},
+	plugins: [organization()]
 });
